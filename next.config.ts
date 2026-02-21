0dcf76f1c10a65a670a900bf128ccc397ed35a28
fatihua <fatihua@gmail.com>
2026-02-22 03:49:17 +0700

    feat(deployment): add production Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["100.116.51.61"],
+  output: "standalone",
 };
 
 export default nextConfig;
